ef1983479acbf26f1300dfa5c2a0edfeed108690
wacefEssaies96 <wacef.stratrait@gmail.com>
2020-04-27 11:41:58 +0100

    update panier

--- app/Http/Controllers/PanierController.php
+++ app/Http/Controllers/PanierController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Panier;
+use App\Produit;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -15,9 +16,18 @@ class PanierController extends Controller
      */
     public function index()
     {
-        $list_panier = Auth::user()->paniers()->get();
+        $list_panier = Auth::user()
+            ->paniers()
+            ->join('produits','paniers.prod_id','=','produits.id')
+            ->select('paniers.id','produits.price','paniers.quantity_prod','produits.name')
+            ->get();
+        $total = 0;
+        foreach($list_panier as $item){
+            $total += ($item['price']*$item->quantity_prod);
+        }
         return view('panier/index',[
             'list_panier' => $list_panier,
+            'total' => $total
         ]);
     }
 
--- resources/views/panier/index.blade.php
+++ resources/views/panier/index.blade.php
@@ -1,19 +1,45 @@
 @extends('layouts.app')
+
 @section('content')
-    <table class="table table-striped">
-    <thead>
-        <th>ProdId</th>
-        <th>Quantity</th>
-   
-    </thead>
-    <tbody>
-    @foreach($list_panier as $panier)
-        <tr>
-            <td><a href="{{ route('panier.show',$panier->id) }}"> {{$panier['id']}}</a></td>
-            <td>{{$panier['quantity_prod']}}</td>
+<div class="main-content-wrapper d-flex clearfix">
+    @include('layouts.navgauche')
+    @if($list_panier)
+        <h3>Prix total : {{ $total }}</h3> 
+        <table class="table table-striped" style=" width:700px; margin-top:100px;">
+            <thead>
+                <th>Name</th>
+                <th>Quantity</th>
+                <th>Price</th>
+                <th>Actions</th>
+            </thead>
+            <tbody>
+            @foreach($list_panier as $panier)
+                <tr>
+                    <td><a href="{{ route('panier.show',$panier->id) }}"> {{$panier['name']}}</a></td>
+                    <td>{{$panier['quantity_prod']}}</td>
+                    <td>{{$panier->price}}</td>
+                    <td>
+                        <form action="{{route('panier.edit',$panier['id'])}}" method="get">
+                            @csrf
+                            <button class="btn btn-outline-success float-left" type="submit">Edit</button>
+                        </form>
+                       
+                        <form action="{{route('panier.destroy',$panier['id'])}}" method="post">
+                            @csrf
+                            @method('DELETE')
+                            <button class="btn btn-outline-danger" type="submit">Delete</button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
             
-        </tr>
-    @endforeach
-    </tbody>
-    </table>
+            </tbody>
+        </table>
+        @else
+            test
+        @endif
+        
+        </div>
+    @include('layouts.footer')  
+ 
 @endsection
